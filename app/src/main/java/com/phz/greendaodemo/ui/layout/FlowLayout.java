2af267cdb11d979ca39b37355f03ce8fe067da60
411593737@qq.com <411593737@qq.com>
2019-08-19 23:00:20 +0800

    Update FlowLayout.java
    完成onMeasure里的逻辑

--- app/src/main/java/com/phz/greendaodemo/ui/layout/FlowLayout.java
+++ app/src/main/java/com/phz/greendaodemo/ui/layout/FlowLayout.java
@@ -66,12 +66,77 @@ public class FlowLayout extends ViewGroup {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        //获取父布局宽度
+        int sizeWidth=MeasureSpec.getSize(widthMeasureSpec);
+        //获取父布局高度
+        int sizeHeight=MeasureSpec.getSize(heightMeasureSpec);
+        //获取父布局宽度模式
+        int modeWidth=MeasureSpec.getMode(widthMeasureSpec);
+        //获取父布局高度模式
+        int modeHeight=MeasureSpec.getMode(heightMeasureSpec);
+
+        //父布局最终宽度
+        int width=0;
+        //父布局最终高度
+        int height=0;
+
+        //当前测量的宽度
+        int lineWidth=0;
+        //当前测量高度
+        int lineHeight=0;
+
+        //当前孩子布局数据
+        int cCount=getChildCount();
+
+        //for循环依次测量孩子布局
+        for (int i=0;i<cCount;i++){
+            View child=getChildAt(i);
+
+            //如果为GONE就跳过本次循环执行下一次循环，我们知道invisible还是会占位置
+            if (child.getVisibility()==View.GONE){
+                continue;
+            }
+
+            //测量孩子布局
+            measureChild(child,widthMeasureSpec,heightMeasureSpec);
+            /**
+             * 调用方法需要子view的布局参数返回MarginLayoutParams而不是默认LayoutParams
+             * widthUsed 父级水平占用额外（可能由其他子集占用）heightUsed同理
+             */
+            /*measureChildWithMargins(child,widthMeasureSpec,0,heightMeasureSpec,0);*/
+
+            MarginLayoutParams lp = (MarginLayoutParams) child.getLayoutParams();
+
+            //注意啊，getWidth方法你是拿不到的值的，在没有onLayout定位之前
+            int childWidth=child.getMeasuredWidth();
+            int childHeight=child.getMeasuredHeight();
+
+            //如果需要占用的宽度超过了父布局能提供的宽度，我们就下一行
+            if (lineWidth+childWidth>sizeWidth-getPaddingLeft()-getPaddingRight()){
+                //比较测量宽度和最终宽度变量，取大的
+                width=Math.max(width,lineWidth);
+                lineWidth=childWidth;
+                height+=lineHeight;
+                lineHeight=childHeight;
+            }
+            else {
+                //没超过，就继续累加测量宽度
+                lineWidth+=childWidth;
+                lineHeight=Math.max(height,lineHeight);
+            }
+        }
+
+        /**
+         * 这里值得一提的是，我没有调用用onMeasure的super.onMeasure(widthMeasureSpec,heightMeasureSpec)
+         * 所以需要调用setMeasuredDimension方法，自己去存储测量宽高。
+         * 如果是特定模式，就直接使用父亲获取的尺寸
+         */
+        setMeasuredDimension(modeWidth==MeasureSpec.EXACTLY?sizeWidth:width,modeHeight==MeasureSpec.EXACTLY?sizeHeight:height);
     }
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        
+
     }
 
     /**
